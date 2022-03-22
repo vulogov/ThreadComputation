4e57bbdccd86d90f456005d6db60cbc3c3cfa01f
vulogov <vlaimir.ulogov@me.com>
2022-03-22 16:24:31 -0600

    Release 1.13

--- ThreadComputation.go
+++ ThreadComputation.go
@@ -19,7 +19,7 @@ var Vars      cmap.Cmap
 var Functions cmap.Cmap
 var Commands  cmap.Cmap
 var Callbacks cmap.Cmap
-var VERSION = "1.12"
+var VERSION = "1.13"
 
 type TCExecListener struct {
   *parser.BaseThreadComputationListener
