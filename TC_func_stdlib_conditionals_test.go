21c1c2950ff7d7ad8d82d91ffac1d215fd1f209c
vulogov <vlaimir.ulogov@me.com>
2022-03-24 17:41:40 -0600

    ifTrue and ifFalse conditional block

---
+++ TC_func_stdlib_conditionals_test.go
@@ -0,0 +1,61 @@
+package ThreadComputation
+
+import (
+	"testing"
+)
+
+func TestStdlibConditionals1(t *testing.T) {
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval("true ifTrue[ 42 ]")
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+	res := tc.GetAsString()
+	if res != "42" {
+		t.Fatalf(" true ifTrue[ 42 ] had failed: %v", res)
+	}
+}
+
+func TestStdlibConditionals2(t *testing.T) {
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval("42 false ifTrue[ 41 ]")
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+	res := tc.GetAsString()
+	if res != "42" {
+		t.Fatalf(" 42 false ifTrue[ 41 ] had failed: %v", res)
+	}
+}
+
+func TestStdlibConditionals3(t *testing.T) {
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval("false ifFalse[ 42 ]")
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+	res := tc.GetAsString()
+	if res != "42" {
+		t.Fatalf(" false ifFalse[ 42 ] had failed: %v", res)
+	}
+}
+
+func TestStdlibConditionals4(t *testing.T) {
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval("42 ifFalse[ 41 ]")
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+	res := tc.GetAsString()
+	if res != "42" {
+		t.Fatalf(" 42 ifFalse[ 41 ] had failed: %v", res)
+	}
+}
