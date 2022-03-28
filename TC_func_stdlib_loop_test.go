3b5cdc42a0297701b9b2a3c3382253d977e2b432
vulogov <vlaimir.ulogov@me.com>
2022-03-28 14:45:58 -0600

    range[34 42 4] loop[stack[#0]] test

--- TC_func_stdlib_loop_test.go
+++ TC_func_stdlib_loop_test.go
@@ -13,3 +13,27 @@ func TestStdlibLoop1(t *testing.T) {
 		t.Fatalf(tc.Error())
 	}
 }
+
+func TestStdlibLoop2(t *testing.T) {
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval("range[34 42 4] loop[#0 println]")
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+}
+
+func TestStdlibLoop3(t *testing.T) {
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval("range[34 42 4] loop[stack[#0]]")
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+	res := tc.GetAsString()
+	if res != "42" {
+		t.Fatalf("range[34 42 4] loop[stack[#0]] had failed: %v", res)
+	}
+}
