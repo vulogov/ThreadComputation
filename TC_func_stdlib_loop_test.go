5940c83bb0dadf909490b885b42591eb0e495816
vulogov <vlaimir.ulogov@me.com>
2022-03-28 12:11:52 -0600

    Fix bogus comments

--- TC_func_stdlib_loop_test.go
+++ TC_func_stdlib_loop_test.go
@@ -7,8 +7,8 @@ import (
 func TestStdlibLoop1(t *testing.T) {
 	// SetVariable("tc.Debuglevel", "debug")
 	tc := Init()
-	SetVariable("tc.Debuglevel", "info")
-	// tc = tc.Eval("3 loop[#0 println]")
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval("3 loop[#0 println]")
 	if tc.Errors() != 0 {
 		t.Fatalf(tc.Error())
 	}
