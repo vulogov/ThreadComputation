0511c1be52c13247222ac10fc5551d5674aeb26b
vulogov <vlaimir.ulogov@me.com>
2022-03-29 13:51:57 -0600

    bug fix:incurrent convert to String of list[]

--- TC_func_stdlib_list.go
+++ TC_func_stdlib_list.go
@@ -1,17 +1,20 @@
 package ThreadComputation
 
 import (
+  log "github.com/sirupsen/logrus"
   "github.com/gammazero/deque"
 )
 
 
 func TCListFunction(l *TCExecListener, name string, q *deque.Deque) (interface{}, error) {
-  var res TCList
+  res := new(TCList)
   for q.Len() > 0 {
     e := q.PopFront()
+    log.Debugf("Pushing %T to list", e)
     res.Q.PushBack(e)
   }
-  return &res, nil
+  log.Debugf("%v elements has been added to the list", res.Q.Len())
+  return res, nil
 }
 
 func init() {
--- TC_list.go
+++ TC_list.go
@@ -2,6 +2,7 @@ package ThreadComputation
 import (
   "fmt"
   "github.com/gammazero/deque"
+  log "github.com/sirupsen/logrus"
 )
 
 type TCList struct {
@@ -20,12 +21,19 @@ func (l *TCList) String() string {
   ret := "list[ "
   for x := 0; x < l.Q.Len(); x++ {
     e := l.Q.At(x)
+    switch e.(type) {
+    case string:
+      ret += fmt.Sprintf(" '%v' ", e)
+      continue
+    }
     fun := GetConverterCallback(e)
     if fun == nil {
+      log.Debugf("list convert: Can not find convert function for %T", e)
       continue
     }
     elem := fun(e, String)
     if elem == nil {
+      log.Debugf("list convert: convert function return nil for %T", e)
       continue
     }
     ret += fmt.Sprintf(" '%v' ", elem)
