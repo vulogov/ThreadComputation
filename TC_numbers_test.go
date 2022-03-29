e8ab08f4e7b522c07070a715bea00ced9775e4c8
vulogov <vlaimir.ulogov@me.com>
2022-03-29 02:09:51 -0600

    numbers[] comparing

--- TC_numbers_test.go
+++ TC_numbers_test.go
@@ -43,3 +43,17 @@ func TestNumbers4(t *testing.T) {
 		t.Fatalf(tc.Error())
 	}
 }
+
+func TestNumbers5(t *testing.T) {
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval("numbers[1 2 3] =[numbers[1 2 3]]")
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+	res := tc.GetAsString()
+	if res != "true" {
+		t.Fatalf("numbers[1 2 3] =[numbers[1 2 3]] had failed: %v", res)
+	}
+}
