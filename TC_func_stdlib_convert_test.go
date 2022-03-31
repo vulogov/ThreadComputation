968e0cb3c5c0d6a25b0bb8611cd0d341d6603785
vulogov <vlaimir.ulogov@me.com>
2022-03-31 17:54:53 -0600

    dict[] now have convert[] get[] set[]

--- TC_func_stdlib_convert_test.go
+++ TC_func_stdlib_convert_test.go
@@ -51,3 +51,13 @@ func TestStdlibConvert4(t *testing.T) {
 		t.Fatalf(tc.Error())
 	}
 }
+
+func TestStdlibConvert5(t *testing.T) {
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval("List convert[dict['answer' 42 'true' true]] println")
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+}
