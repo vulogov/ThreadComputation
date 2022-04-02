66ecbbd6c45b8a7f2256edbb79d1ffa1c7bf773d
vulogov <vlaimir.ulogov@me.com>
2022-04-01 20:14:45 -0600

    compare for bin[]

--- TC_binary_test.go
+++ TC_binary_test.go
@@ -33,3 +33,33 @@ func TestBinary2(t *testing.T) {
     t.Fatalf("%v is failed: %v", code, res)
   }
 }
+
+func TestBinary3(t *testing.T) {
+	code := "bin['Hello'] =[bin['Hello']]"
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval(code)
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+	res := tc.GetAsString()
+  if res != "true" {
+    t.Fatalf("%v is failed: %v", code, res)
+  }
+}
+
+func TestBinary4(t *testing.T) {
+	code := "bin['Hello'] =['Hello']"
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval(code)
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+	res := tc.GetAsString()
+  if res != "true" {
+    t.Fatalf("%v is failed: %v", code, res)
+  }
+}
