e51f23b0ad14c31e9f55efdfb169fce203760e19
vulogov <vlaimir.ulogov@me.com>
2022-04-07 16:49:57 -0600

    if there is a key in context 'p' the ;[] will create a value with probability p

--- TC_func_stdlib_stack_backsend.go
+++ TC_func_stdlib_stack_backsend.go
@@ -6,10 +6,24 @@ import (
 )
 
 func TCStackBackSendFunction(l *TCExecListener, name string, q *deque.Deque) (interface{}, error) {
+  p := l.TC.GetContext("p")
   for q.Len() > 0 {
     e := q.PopFront()
     log.Debugf("\";\" Backsending the data: %T", e)
-    ReturnFromFunction(l, ";", e)
+    if p == nil {
+      ReturnFromFunction(l, ";", e)
+    } else {
+      switch p.(type) {
+      case float64:
+        ReturnFromFunction(l, ";", MakeValue(e, float32(p.(float64)), 0))
+      case int64:
+        ReturnFromFunction(l, ";", MakeValue(e, float32(p.(int64)), 0))
+      case *TCValue:
+        ReturnFromFunction(l, ";", MakeValue(e, p.(*TCValue).P, 0))
+      default:
+        ReturnFromFunction(l, ";", e)
+      }
+    }
   }
   return nil, nil
 }
--- TC_func_stdlib_stack_backsend_test.go
+++ TC_func_stdlib_stack_backsend_test.go
@@ -48,3 +48,18 @@ func TestStdlibStackBackSend3(t *testing.T) {
           t.Fatalf("%v failed: %v", code, res)
   }
 }
+
+func TestStdlibStackBackSend4(t *testing.T) {
+	code := "local[let['p' 50]] stack [ ;[41] +[1] ] value"
+	// SetVariable("tc.Debuglevel", "debug")
+	tc := Init()
+	// SetVariable("tc.Debuglevel", "info")
+	tc = tc.Eval(code)
+	if tc.Errors() != 0 {
+		t.Fatalf(tc.Error())
+	}
+	res := tc.GetAsString()
+  if res != "42" {
+          t.Fatalf("%v failed: %v", code, res)
+  }
+}
